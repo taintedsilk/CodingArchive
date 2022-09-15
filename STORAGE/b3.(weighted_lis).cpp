67de031e4e476f9753ac3556bf5716581d3c6f1d
taintedsilk <92366795+taintedsilk@users.noreply.github.com>
2022-09-15 20:09:25 +0700

    Update and rename b3.cpp to b3.(weighted_lis).cpp

--- STORAGE/b3.(weighted_lis).cpp
+++ STORAGE/b3.(weighted_lis).cpp
@@ -1,5 +1,7 @@
 #include <bits/stdc++.h>
-
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
 using namespace std;
 
 struct qhash
@@ -33,9 +35,8 @@ typedef double db;
 typedef long double ldb;
 typedef unordered_map<ll, ll, qhash> llumap;
 typedef map<ll, ll> llmap;
+typedef vector<ll> llv;
 
-#define fi first
-#define se second
 #define mp(p1, p2) make_pair(p1, p2)
 #define f(i, n) for (ll i = 0; i < n; i += 1)
 #define F(i, n, inc) for (ll i = 0; i < n; i += inc)
@@ -45,50 +46,52 @@ typedef map<ll, ll> llmap;
 #define Fd(i, start, end, inc) for (int i = start; i >= end; i -= inc)
 #define itr iterator
 #define BIT(var,pos) ((bool) ((var) & (1<<(pos))))
-#define pb push_back
 //#define ordered_set tree<pair<ll, ll>, null_type,less<pair<ll, ll>>, rb_tree_tag, tree_order_statistics_node_update>
-vector<pair<ll, ll>>arr;
-ll dp[1000001];
-ll tree[2000000];
-void update(ll l, ll r, ll val) {
-    for (l += n, r += n; l < r; l >>= 1, r >>= 1) {
-        if (l & 1) tree[l++] = max(tree[l - 1], val);
-        if (r & 1) tree[++r] = max(tree[r], val);
-    }
-}
 
-
-
-void solve() {
+ll arr[100000];
+ll weight[100000];
+void solve()
+{
     ll n;
     cin >> n;
-    f(i, n) {
-        ll a, b;
-        cin >> a >> b;
-        arr.push_back({a, b});
-    }
-
-    sort(arr.begin(), arr.end());
-    f(i, n) {
-        pair<ll, ll>temp = {arr[i].se, -1};
-        auto it = lower_bound(arr.begin(), arr.end(), temp);
-        ll idx = (it - arr.begin());
-        dp[idx] = max(dp[idx], dp[i] + 1);
-        f(i, n) {
-            cout << dp[i] << " ";
+    f(i, n)
+        cin >> arr[i];
+    f(i, n)
+        cin >> weight[i];
+    set<vector<ll>>s;
+    f(i, n)
+    {
+        auto it = s.lower_bound(llv{arr[i], LLONG_MAX, LLONG_MAX});
+        ll val = 0;
+        if (it == s.begin())
+        {
+            val = weight[i];
+            s.insert(llv{arr[i], weight[i], 1});
         }
-        cout << "\n";
+        else
+        {
+            it = prev(it);
+            val = (*it)[1] + weight[i];
+            s.insert(llv{arr[i], (*it)[1] + weight[i], (*it)[2] + 1});
+            it++;
+            it++;
+
+        }
+        while (it != s.end())
+        {
+
+            if ((*it)[1] < val) it = s.erase(it);
+            else break;
+        }
     }
-
-
+    cout << (*prev(s.end()))[2] << "\n";
 }
-
 int main()
 {
 
     //freopen("pushing.INP", "r" ,stdin); freopen("pushing.OUT", "w", stdout);
-    //std::ios_base::sync_with_stdio(0);
-    //std::cin.tie(0);
+    std::ios_base::sync_with_stdio(0);
+    std::cin.tie(0);
     long long test = 1;
 
     //cin >> test;
